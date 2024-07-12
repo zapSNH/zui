ced4c3acd110fa892004ddaf5884b1d67d08d1d0
ZapSNH <134786889+zapSNH@users.noreply.github.com>
2024-07-12 15:33:53 +0800

    formatting

--- AdaptiveNavball.cs
+++ AdaptiveNavball.cs
@@ -3,81 +3,81 @@ using UnityEngine;
 
 namespace ZUI
 {
-    [KSPAddon(KSPAddon.Startup.Flight, false)]
-    public class AdaptiveNavball : MonoBehaviour
-    {
-        internal static AdaptiveNavball instance;
-        internal UrlDir.UrlConfig[] navballConfigs;
-        private string[] navballPaths = new string[3];
-        private bool[] navballExists = new bool[3];
-        private Texture2D navballTexture;
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	public class AdaptiveNavball : MonoBehaviour
+	{
+		internal static AdaptiveNavball instance;
+		internal UrlDir.UrlConfig[] navballConfigs;
+		private string[] navballPaths = new string[3];
+		private bool[] navballExists = new bool[3];
+		private Texture2D navballTexture;
 
-        public void Start()
-        {
-            instance = this;
-            navballConfigs = GameDatabase.Instance.GetConfigs("ZUINavBall");
-            foreach (UrlDir.UrlConfig config in navballConfigs)
-            {
-                if (config.config.HasValue("navball_surface"))
-                {
-                    navballPaths[0] = config.config.GetValue("navball_surface");
-                    navballExists[0] = true;
-                }
-                if (config.config.HasValue("navball_orbit"))
-                {
-                    navballPaths[1] = config.config.GetValue("navball_orbit");
-                    navballExists[1] = true;
-                }
-                if (config.config.HasValue("navball_target"))
-                {
-                    navballPaths[2] = config.config.GetValue("navball_target");
-                    navballExists[2] = true;
-                }
-            }
-            foreach (Texture2D tex in (Texture2D[])(object)Resources.FindObjectsOfTypeAll(typeof(Texture2D)))
-            {
-                if (tex.name == "NavBall")
-                {
-                    navballTexture = tex;
-                    Debug.Log("[ZUI] Found NavBall texture!");
-                    break;
-                }
-            }
-            Debug.Log("[ZUI] navballExists: Surface: " + navballExists[0] + ", Orbit: " + navballExists[1] + ", Target: " + navballExists[2]);
-            Debug.Log("[ZUI] navballPaths: Surface: " + navballPaths[0] + ", Orbit: " + navballPaths[1] + ", Target:" + navballPaths[2]);
-            ChangeNavball(new FlightGlobals.SpeedDisplayModes());
-            GameEvents.onSetSpeedMode.Add(ChangeNavball);
-        }
+		public void Start()
+		{
+			instance = this;
+			navballConfigs = GameDatabase.Instance.GetConfigs("ZUINavBall");
+			foreach (UrlDir.UrlConfig config in navballConfigs)
+			{
+				if (config.config.HasValue("navball_surface"))
+				{
+					navballPaths[0] = config.config.GetValue("navball_surface");
+					navballExists[0] = true;
+				}
+				if (config.config.HasValue("navball_orbit"))
+				{
+					navballPaths[1] = config.config.GetValue("navball_orbit");
+					navballExists[1] = true;
+				}
+				if (config.config.HasValue("navball_target"))
+				{
+					navballPaths[2] = config.config.GetValue("navball_target");
+					navballExists[2] = true;
+				}
+			}
+			foreach (Texture2D tex in (Texture2D[])(object)Resources.FindObjectsOfTypeAll(typeof(Texture2D)))
+			{
+				if (tex.name == "NavBall")
+				{
+					navballTexture = tex;
+					Debug.Log("[ZUI] Found NavBall texture!");
+					break;
+				}
+			}
+			Debug.Log("[ZUI] navballExists: Surface: " + navballExists[0] + ", Orbit: " + navballExists[1] + ", Target: " + navballExists[2]);
+			Debug.Log("[ZUI] navballPaths: Surface: " + navballPaths[0] + ", Orbit: " + navballPaths[1] + ", Target:" + navballPaths[2]);
+			ChangeNavball(new FlightGlobals.SpeedDisplayModes());
+			GameEvents.onSetSpeedMode.Add(ChangeNavball);
+		}
 
-        internal void ChangeNavball(FlightGlobals.SpeedDisplayModes speedMode)
-        {
-            Debug.Log("[ZUI] Switching Navball mode to " + FlightGlobals.speedDisplayMode);
-            switch (FlightGlobals.speedDisplayMode.ToString())
-            {
-                case "Surface":
-                    if (navballExists[0])
-                    {
-                        ImageConversion.LoadImage(navballTexture, File.ReadAllBytes(KSPUtil.ApplicationRootPath + navballPaths[0]));
-                    }
-                    break;
-                case "Orbit":
-                    if (navballExists[1])
-                    {
-                        ImageConversion.LoadImage(navballTexture, File.ReadAllBytes(KSPUtil.ApplicationRootPath + navballPaths[1]));
-                    }
-                    break;
-                case "Target":
-                    if (navballExists[2])
-                    {
-                        ImageConversion.LoadImage(navballTexture, File.ReadAllBytes(KSPUtil.ApplicationRootPath + navballPaths[2]));
-                    }
-                    break;
-            }
-        }
+		internal void ChangeNavball(FlightGlobals.SpeedDisplayModes speedMode)
+		{
+			Debug.Log("[ZUI] Switching Navball mode to " + FlightGlobals.speedDisplayMode);
+			switch (FlightGlobals.speedDisplayMode.ToString())
+			{
+				case "Surface":
+					if (navballExists[0])
+					{
+						ImageConversion.LoadImage(navballTexture, File.ReadAllBytes(KSPUtil.ApplicationRootPath + navballPaths[0]));
+					}
+					break;
+				case "Orbit":
+					if (navballExists[1])
+					{
+						ImageConversion.LoadImage(navballTexture, File.ReadAllBytes(KSPUtil.ApplicationRootPath + navballPaths[1]));
+					}
+					break;
+				case "Target":
+					if (navballExists[2])
+					{
+						ImageConversion.LoadImage(navballTexture, File.ReadAllBytes(KSPUtil.ApplicationRootPath + navballPaths[2]));
+					}
+					break;
+			}
+		}
 
-        public void OnDisable()
-        {
-            GameEvents.onSetSpeedMode.Remove(ChangeNavball);
-        }
-    }
+		public void OnDisable()
+		{
+			GameEvents.onSetSpeedMode.Remove(ChangeNavball);
+		}
+	}
 }
--- Transformer.cs
+++ Transformer.cs
@@ -9,122 +9,122 @@ using UnityEngine.UI;
 // Partially based on wheeeUI and Visual Studio's autocomplete feature
 namespace ZUI
 {
-    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
-    public class Transformer : MonoBehaviour
-    {
-        internal static Transformer instance;
-        internal UrlDir.UrlConfig[] transforms;
-        internal GameObject[] transformObjects;
-        internal string[] relativeTransform; // this will turn out okay
-        public Vector3[] translateAmounts;
-        public Vector3[] rotateAmounts; // rotate will always be !relative
-        // i'll add scale eventually
+	[KSPAddon(KSPAddon.Startup.EveryScene, false)]
+	public class Transformer : MonoBehaviour
+	{
+		internal static Transformer instance;
+		internal UrlDir.UrlConfig[] transforms;
+		internal GameObject[] transformObjects;
+		internal string[] relativeTransform; // this will turn out okay
+		public Vector3[] translateAmounts;
+		public Vector3[] rotateAmounts; // rotate will always be !relative
+		// i'll add scale eventually
 
-        private bool debugMode = false;
+		private bool debugMode = false;
 
-        public void Start()
-        {
-            instance = this;
-            transforms = GameDatabase.Instance.GetConfigs("ZUITransform");
-            transformObjects = new GameObject[transforms.Length];
-            translateAmounts = new Vector3[transforms.Length];
-            rotateAmounts = new Vector3[transforms.Length];
-            relativeTransform = new string[transforms.Length];
+		public void Start()
+		{
+			instance = this;
+			transforms = GameDatabase.Instance.GetConfigs("ZUITransform");
+			transformObjects = new GameObject[transforms.Length];
+			translateAmounts = new Vector3[transforms.Length];
+			rotateAmounts = new Vector3[transforms.Length];
+			relativeTransform = new string[transforms.Length];
 
-            int i = 0;
-            foreach (UrlDir.UrlConfig config in transforms)
-            {
-                if (!config.config.HasValue("target"))
-                {
-                    Debug.Log("[ZUI] Node does not have a transform target!");
-                    continue;
-                }
-                string target = config.config.GetValue("target");
-                float[] translate = new float[3];
-                GameObject gameObject = GameObject.Find(target);
+			int i = 0;
+			foreach (UrlDir.UrlConfig config in transforms)
+			{
+				if (!config.config.HasValue("target"))
+				{
+					Debug.Log("[ZUI] Node does not have a transform target!");
+					continue;
+				}
+				string target = config.config.GetValue("target");
+				float[] translate = new float[3];
+				GameObject gameObject = GameObject.Find(target);
 
-                // Some GameObjects return null even tho they exist. Calling the Start() function again fixes this.
-                if (gameObject != null)
-                {
-                    transformObjects[i] = gameObject;
+				// Some GameObjects return null even tho they exist. Calling the Start() function again fixes this.
+				if (gameObject != null)
+				{
+					transformObjects[i] = gameObject;
 
-                    if (config.config.HasValue("relative"))
-                    {
-                        relativeTransform[i] = config.config.GetValue("relative");
-                    }
-                    translate[0] = config.config.HasValue("translate_x") ? Convert.ToSingle(config.config.GetValue("translate_x")) : 0f;
-                    translate[1] = config.config.HasValue("translate_y") ? Convert.ToSingle(config.config.GetValue("translate_y")) : 0f;
-                    translate[2] = config.config.HasValue("translate_z") ? Convert.ToSingle(config.config.GetValue("translate_z")) : 0f;
-                    rotateAmounts[i] = config.config.HasValue("rotate")
-                        ? new Vector3(0f, 0f, Convert.ToSingle(config.config.GetValue("rotate")))
-                        : new Vector3(0f, 0f, 0f);
-                    translateAmounts[i] = new Vector3(translate[0], translate[1], translate[2]);
-                    Debug.Log("[ZUI] target: " + gameObject.ToString() + ", translateAmounts: " + translateAmounts[i] + ", rotateAmounts: " + rotateAmounts[i]);
-                } else
-                {
-                    Debug.Log("[ZUI] Invalid transform target! (" + target + ")");
-                    continue;
-                }
-                i++;
-            }
+					if (config.config.HasValue("relative"))
+					{
+						relativeTransform[i] = config.config.GetValue("relative");
+					}
+					translate[0] = config.config.HasValue("translate_x") ? Convert.ToSingle(config.config.GetValue("translate_x")) : 0f;
+					translate[1] = config.config.HasValue("translate_y") ? Convert.ToSingle(config.config.GetValue("translate_y")) : 0f;
+					translate[2] = config.config.HasValue("translate_z") ? Convert.ToSingle(config.config.GetValue("translate_z")) : 0f;
+					rotateAmounts[i] = config.config.HasValue("rotate")
+						? new Vector3(0f, 0f, Convert.ToSingle(config.config.GetValue("rotate")))
+						: new Vector3(0f, 0f, 0f);
+					translateAmounts[i] = new Vector3(translate[0], translate[1], translate[2]);
+					Debug.Log("[ZUI] target: " + gameObject.ToString() + ", translateAmounts: " + translateAmounts[i] + ", rotateAmounts: " + rotateAmounts[i]);
+				} else
+				{
+					Debug.Log("[ZUI] Invalid transform target! (" + target + ")");
+					continue;
+				}
+				i++;
+			}
 
-            // This could probably be merged with the loop above but just in
-            // case I find a gameObject that can only be moved in Update() or
-            // something then it would be very easy to move this loop there.
-            i = 0;
-            foreach (GameObject gameObject in transformObjects)
-            {
-                if (gameObject != null)
-                {
-                    switch (relativeTransform[i])
-                    {
-                        case "yes":
-                        case "YES":
-                        case "true":
-                        case "TRUE":
-                            gameObject.transform.localPosition += translateAmounts[i];
-                            break;
-                        default:
-                            gameObject.transform.localPosition = translateAmounts[i];
-                            break;
-                    }
-                    gameObject.transform.localEulerAngles = rotateAmounts[i];
-                }
-                i++;
-            }
+			// This could probably be merged with the loop above but just in
+			// case I find a gameObject that can only be moved in Update() or
+			// something then it would be very easy to move this loop there.
+			i = 0;
+			foreach (GameObject gameObject in transformObjects)
+			{
+				if (gameObject != null)
+				{
+					switch (relativeTransform[i])
+					{
+						case "yes":
+						case "YES":
+						case "true":
+						case "TRUE":
+							gameObject.transform.localPosition += translateAmounts[i];
+							break;
+						default:
+							gameObject.transform.localPosition = translateAmounts[i];
+							break;
+					}
+					gameObject.transform.localEulerAngles = rotateAmounts[i];
+				}
+				i++;
+			}
 
-            UrlDir.UrlConfig[] ZUISettings = GameDatabase.Instance.GetConfigs("ZUISettings");
-            foreach (UrlDir.UrlConfig config in ZUISettings)
-            {
-                if (config.config.HasValue("debug_mode"))
-                {
-                    if (config.config.GetValue("debug_mode") == "true") // too lazy to find out if this doesn't error when these if statements are combined.
-                    {
-                        debugMode = true;
-                    }
-                }
-            }
-        }
-        public void Update()
-        {
-            if (debugMode)
-            {
-                if (Input.GetKeyUp(KeyCode.E))
-                {
-                    GameObject[] gameObjects = FindObjectsOfType<GameObject>();
-                    foreach (GameObject gameObject in gameObjects)
-                    {
-                        if (gameObject != null)
-                        {
-                            Debug.Log("[ZUI] Name: " + gameObject.name + " | Translate: " + gameObject.transform.localPosition.ToString() + " | Rotate: " + gameObject.transform.localPosition.ToString());
-                        }
-                    }
-                }
-                //if (Input.GetKeyUp(KeyCode.Q))
-                //{
-                //    Start(); // Unfortunately the configs don't get reloaded so all this does is nudge the textures more
-                //}
-            }
-        }
-    }
+			UrlDir.UrlConfig[] ZUISettings = GameDatabase.Instance.GetConfigs("ZUISettings");
+			foreach (UrlDir.UrlConfig config in ZUISettings)
+			{
+				if (config.config.HasValue("debug_mode"))
+				{
+					if (config.config.GetValue("debug_mode") == "true") // too lazy to find out if this doesn't error when these if statements are combined.
+					{
+						debugMode = true;
+					}
+				}
+			}
+		}
+		public void Update()
+		{
+			if (debugMode)
+			{
+				if (Input.GetKeyUp(KeyCode.E))
+				{
+					GameObject[] gameObjects = FindObjectsOfType<GameObject>();
+					foreach (GameObject gameObject in gameObjects)
+					{
+						if (gameObject != null)
+						{
+							Debug.Log("[ZUI] Name: " + gameObject.name + " | Translate: " + gameObject.transform.localPosition.ToString() + " | Rotate: " + gameObject.transform.localPosition.ToString());
+						}
+					}
+				}
+				//if (Input.GetKeyUp(KeyCode.Q))
+				//{
+				//	Start(); // Unfortunately the configs don't get reloaded so all this does is nudge the textures more
+				//}
+			}
+		}
+	}
 }
